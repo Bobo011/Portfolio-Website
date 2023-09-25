eda67f699c54987ea962d8bec56384d7f53dc2e8
George <alximik15@gmail.com>
2023-09-25 16:07:57 +0400

    ddds

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { github, netlify } from "../assets";
 import { SectionWrapper } from "../hoc";
@@ -18,13 +17,14 @@ const ProjectCard = ({
   source_code_link2,
 }) => {
   // Check if the device has a touch screen (mobile or tablet)
-  const isTouchDevice = window.matchMedia("(hover: none)").matches;
+  const isTouchDevice =
+    "ontouchstart" in window || navigator.maxTouchPoints > 0;
 
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <div>
       {isTouchDevice ? (
-        // Render without Tilt on touch devices
-        <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        // Render without Tilt and motion on touch devices
+        <div className="bg-tertiary p-5 rounded-2xl w-full">
           <div className="relative w-full h-[230px]">
             <img
               src={image}
@@ -73,86 +73,113 @@ const ProjectCard = ({
           </div>
         </div>
       ) : (
-        // Render with Tilt on non-touch devices
-        <Tilt
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-        >
-          <div className="relative w-full h-[230px]">
-            <img
-              src={image}
-              alt="project_image"
-              className="w-full h-full object-cover rounded-2xl"
-            />
+        // Render with Tilt and motion on non-touch devices
+        <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+          <Tilt
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450,
+            }}
+            className="bg-tertiary p-5 rounded-2xl w-full"
+          >
+            <div className="relative w-full h-[230px]">
+              <img
+                src={image}
+                alt="project_image"
+                className="w-full h-full object-cover rounded-2xl"
+              />
 
-            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-              <div
-                onClick={() => window.open(source_code_link1, "_blank")}
-                className="black-gradient mr-3 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              >
-                <img
-                  src={github}
-                  alt="source code"
-                  className="w-1/2 h-1/2 object-contain"
-                />
-              </div>
-              <div
-                onClick={() => window.open(source_code_link2, "_blank")}
-                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              >
-                <img
-                  src={netlify}
-                  alt="source code"
-                  className="w-1/2 h-1/2 object-contain"
-                />
+              <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+                <div
+                  onClick={() => window.open(source_code_link1, "_blank")}
+                  className="black-gradient mr-3 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                >
+                  <img
+                    src={github}
+                    alt="source code"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
+                <div
+                  onClick={() => window.open(source_code_link2, "_blank")}
+                  className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                >
+                  <img
+                    src={netlify}
+                    alt="source code"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="mt-5">
-            <h3 className="text-white font-bold text-[24px]">{name}</h3>
-            <p className="mt-2 text-secondary text-[14px]">{description}</p>
-          </div>
+            <div className="mt-5">
+              <h3 className="text-white font-bold text-[24px]">{name}</h3>
+              <p className="mt-2 text-secondary text-[14px]">{description}</p>
+            </div>
 
-          <div className="mt-4 flex flex-wrap gap-2">
-            {tags.map((tag) => (
-              <p
-                key={`${name}-${tag.name}`}
-                className={`text-[14px] ${tag.color}`}
-              >
-                #{tag.name}
-              </p>
-            ))}
-          </div>
-        </Tilt>
+            <div className="mt-4 flex flex-wrap gap-2">
+              {tags.map((tag) => (
+                <p
+                  key={`${name}-${tag.name}`}
+                  className={`text-[14px] ${tag.color}`}
+                >
+                  #{tag.name}
+                </p>
+              ))}
+            </div>
+          </Tilt>
+        </motion.div>
       )}
-    </motion.div>
+    </div>
   );
 };
 
 const Works = () => {
+  // Check if the device has a touch screen (mobile or tablet)
+  const isTouchDevice =
+    "ontouchstart" in window || navigator.maxTouchPoints > 0;
+
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      {isTouchDevice ? (
+        // Render without motion on touch devices
+        <div>
+          <p className={`${styles.sectionSubText} `}>My work</p>
+          <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        </div>
+      ) : (
+        // Render with motion on non-touch devices
+        <motion.div variants={textVariant()}>
+          <p className={`${styles.sectionSubText} `}>My work</p>
+          <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        </motion.div>
+      )}
 
       <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          Following projects showcase my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-        </motion.p>
+        {isTouchDevice ? (
+          // Render without motion on touch devices
+          <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+            Following projects showcase my skills and experience through
+            real-world examples of my work. Each project is briefly described
+            with links to code repositories and live demos in it. It reflects my
+            ability to solve complex problems, work with different technologies,
+            and manage projects effectively.
+          </p>
+        ) : (
+          // Render with motion on non-touch devices
+          <motion.p
+            variants={fadeIn("", "", 0.1, 1)}
+            className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          >
+            Following projects showcase my skills and experience through
+            real-world examples of my work. Each project is briefly described
+            with links to code repositories and live demos in it. It reflects my
+            ability to solve complex problems, work with different technologies,
+            and manage projects effectively.
+          </motion.p>
+        )}
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
