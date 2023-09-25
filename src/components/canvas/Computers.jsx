379e2ff6863530ab98c0c59ef093f7919e996b5c
George <alximik15@gmail.com>
2023-09-25 12:12:06 +0400

    ds

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -40,7 +40,7 @@ const ComputersCanvas = () => {
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
-
+console.log("isMobile:", mediaQuery.matches);
     // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
