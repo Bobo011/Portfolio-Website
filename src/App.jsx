4d1c49797a6392605f3e57fd930b52e6f014e4b2
George <alximik15@gmail.com>
2023-09-25 15:12:58 +0400

    tts

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,61 @@
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the window width
+    const handleWindowResize = () => {
+      setIsMobile(window.innerWidth <= 500); // Adjust the threshold as needed
+    };
+
+    // Initial check
+    handleWindowResize();
+
+    // Add the resize event listener
+    window.addEventListener("resize", handleWindowResize);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
         <About />
         <Experience />
-        <Tech />
+
+        {/* Conditionally render the Tech component */}
+        {!isMobile && <Tech />}
+
         <Works />
         {/* <Feedbacks /> */}
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the window width
+    const handleWindowResize = () => {
+      setIsMobile(window.innerWidth <= 500); // Adjust the threshold as needed
+    };
+
+    // Initial check
+    handleWindowResize();
+
+    // Add the resize event listener
+    window.addEventListener("resize", handleWindowResize);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  // Render the BallCanvas components conditionally
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) =>
+        // Only render BallCanvas if not on a mobile-sized screen
+        isMobile ? null : (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        )
+      )}
     </div>
   );
 };
